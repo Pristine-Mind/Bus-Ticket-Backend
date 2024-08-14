85333e431d3072334a37d1b938e9bae1a6940d4d
k9845 <khatririshi2430@gmail.com>
2024-08-14 21:52:59 +0545

    Add serializers for user model

---
+++ user/serializers.py
@@ -0,0 +1,74 @@
+from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.db import transaction
+from django.utils.translation import gettext
+
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class UserPasswordSerializer(serializers.ModelSerializer):
+    old_password = serializers.CharField(required=True, write_only=True)
+    new_password = serializers.CharField(required=True, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["old_password", "new_password"]
+
+    def validate_old_password(self, password) -> str:
+        if not self.instance.check_password(password):
+            raise serializers.ValidationError("The password is invalid.")
+        return password
+
+    def validate_new_password(self, password) -> str:
+        validate_password(password)
+        return password
+
+    def save(self, **kwargs):
+        self.instance.set_password(self.validated_data["new_password"])
+        self.instance.save()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(required=True, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["email", "first_name", "last_name", "password"]
+
+    def validate_password(self, password) -> str:
+        validate_password(password)
+        return password
+
+    def validate_email(self, email) -> str:
+        if User.objects.filter(email__iexact=email).exists():
+            raise serializers.ValidationError("The email is already taken.")
+        return email
+
+    def save(self, **kwargs):
+        with transaction.atomic():
+            instance = User.objects.create_user(
+                first_name=self.validated_data.get("first_name", ""),
+                last_name=self.validated_data.get("last_name", ""),
+                username=self.validated_data["email"],
+                email=self.validated_data["email"],
+                password=self.validated_data["password"],
+                is_active=False,
+            )
+        return instance
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True, write_only=True)
+    password = serializers.CharField(required=True, write_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get("email", "")
+        if User.objects.filter(email__iexact=email, is_active=False).exists():
+            raise serializers.ValidationError(gettext("Request an admin to activate your account."))
+        user = authenticate(email=email, password=attrs.get("password", ""))
+        if not user:
+            raise serializers.ValidationError("The email or password is invalid.")
+        attrs.update(dict(user=user))
+        return attrs
